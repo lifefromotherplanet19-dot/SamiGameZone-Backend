a3660a91257b5f72615047aee2449335e181fbab
Sami game zone <lifefromotherplanet19@gmail.com>
2026-07-02 14:12:41 +0300

    Refactor server to use environment variable for port

--- server.js
+++ server.js
@@ -92,6 +92,8 @@ app.delete("/api/games/:id", verifyToken, (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
